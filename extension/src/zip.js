d68f0b00dc2d23252de89d9cd61134e29a2be976
OfficialCRUGG <ich@crugg.de>
2024-12-18 03:46:21 +0100

    chore: update & fix packaging flow

--- extension/src/zip.js
+++ extension/src/zip.js
@@ -1,10 +1,11 @@
-import gulp from 'gulp'
-import zip from 'gulp-zip'
-import { createRequire } from 'module'
-const require = createRequire(import.meta.url)
-const manifest = require('../build/manifest.json')
+import gulp from "gulp";
+import zip from "gulp-zip";
+import { createRequire } from "module";
+const require = createRequire(import.meta.url);
+const browser = process.env.BROWSER;
+const manifest = require(`../build/${browser}/manifest.json`);
 
 gulp
-  .src('build/**')
-  .pipe(zip(`${manifest.name.replaceAll(' ', '-')}-${manifest.version}.zip`))
-  .pipe(gulp.dest('package'))
+  .src(`build/${browser}/**`)
+  .pipe(zip(`asutils-${manifest.version}-${browser}.zip`))
+  .pipe(gulp.dest("package"));
